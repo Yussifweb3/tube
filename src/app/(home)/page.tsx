b9efa7f7263c11b45c2d8cd903a67b2b9fff1692
inuldev <inuldev0@gmail.com>
2025-02-22 02:03:43 +0700

    trpc setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <div>I will load videos in the future!</div>;
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { HydrateClient, trpc } from "@/trpc/server";
+
+import { PageClient } from "./client";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "InTube" });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
